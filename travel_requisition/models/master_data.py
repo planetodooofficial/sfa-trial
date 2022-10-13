3907022ae9b0cac33b4d140738d7f4c7112e8335
amey204 <amey.planetodoo@gmail.com>
2022-10-13 18:41:14 +0530

    adding changes2

--- travel_requisition/models/master_data.py
+++ travel_requisition/models/master_data.py
@@ -3,19 +3,23 @@ from odoo import models, fields, api, _
 
 class DesignationMaster(models.Model):
     _name = 'designation.master'
+    _rec_name = 'designation_value'
     designation_value = fields.Char(string='Designation')
 
 
 class GradeMaster(models.Model):
     _name = 'grade.master'
+    _rec_name = 'grade_new'
     grade_new = fields.Char(string='Grade')
 
 
 class CadreMaster(models.Model):
     _name = 'cadre.master'
+    _rec_name = 'cadre_new'
     cadre_new = fields.Char(string='Cadre')
 
 
 class GradeTitleMaster(models.Model):
     _name = 'grade.title.master'
+    _rec_name = 'grade_title_line'
     grade_title_line = fields.Char(string='Grade Title')
