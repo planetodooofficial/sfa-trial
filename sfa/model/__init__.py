a204fcd21792a173df0c76d1544e5ca3a813246d
amey204 <sanjeev.planetodoo@gmail.com>
2022-10-17 14:57:58 +0530

    added

--- sfa/model/__init__.py
+++ sfa/model/__init__.py
@@ -1,8 +1,8 @@
-
 from . import analytic_account
 from . import account
 from . import res_partner
 from . import cost_profit_center
 from . import import_vendor_bills
 from . import partner
+from . import hr_payslip
 
