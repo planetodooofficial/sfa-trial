fe36077f05b5bb66f981bce32f4e67fc541bd39c
amey204 <amey.planetodoo@gmail.com>
2022-10-21 14:50:56 +0530

    add changes1

--- travel_requisition/controllers/expense_portal.py
+++ travel_requisition/controllers/expense_portal.py
@@ -5,21 +5,28 @@ from odoo.addons.portal.controllers.portal import CustomerPortal
 
 class AllExpense(http.Controller):
     @http.route('/my/MyReimbursement/', website=True, auth='public')
-    def display_expenses(self, **kw):
-        # return "Hello"
-        # return http.request.render('travel_requisition.expenses', {
-        #     'expenses': ['math', 'english', 'marathi'],
-        # })
-        all_expenses = http.request.env['hr.expense'].search([])
+    def display_expenses(self, sortby=None, **kw):
+        searchbar_sortings = {
+            'date': {'label': _('Expense Date'), 'order': 'date desc'},
+            'name': {'label': _('Name'), 'order': 'name'},
+        }
+        if not sortby:
+            sortby = 'date'
+        order = searchbar_sortings[sortby]['order']
+
+        all_expenses = http.request.env['hr.expense'].search([], order=order)
         return http.request.render('travel_requisition.portal_all_expenses_list', {
             'expenses': all_expenses,
             'page_name': 'expense',
+            'searchbar_sortings': searchbar_sortings,
+            'sortby': sortby
         })
 
     @http.route('/my/MyReimbursement/<model("hr.expense"):expense>', auth='public', website=True)
     def display_expense_detail(self, expense):
         return http.request.render('travel_requisition.expense_detail', {
             'expense': expense,
+            'page_name': 'expense',
         })
 
 
