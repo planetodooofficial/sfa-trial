c06212cc78883867db5610d07031750dd7107286
amey204 <amey.planetodoo@gmail.com>
2022-11-01 19:27:40 +0530

    add changes

--- travel_requisition/controllers/my_reimbursement_portal.py
+++ travel_requisition/controllers/my_reimbursement_portal.py
@@ -1,6 +1,7 @@
 from odoo import http, _
 from odoo.http import content_disposition, Controller, request, route
 from odoo.addons.portal.controllers.portal import CustomerPortal
+from datetime import date
 
 
 class AllMyExpense(http.Controller):
@@ -30,6 +31,58 @@ class AllMyExpense(http.Controller):
             'rexpense': rexpense,
             'page_name': 'pexpense',
         })
+
+    @http.route('/create/MyReimbursement', website=True, auth='public')
+    def myreimbursement_form(self, **kw):
+        userid = request.env.user.employee_id
+
+        r_product = request.env['product.product'].sudo().search(
+            [('travel_requisition', '=', False), ('can_be_expensed', '=', True)])
+        cdate = date.today()
+
+        account_id_def = request.env['account.account'].sudo().search(
+            [('internal_type', '=', 'other'), ('code', '=', '210700')])
+        account_id = request.env['account.account'].sudo().search([('internal_type', '=', 'other')])
+
+        currency_name = request.env['res.currency'].sudo().search([])
+        tax_name = request.env['account.tax'].sudo().search(
+            [('type_tax_use', '=', 'purchase'), ('price_include', '=', True)])
+        analytic_account = request.env['account.analytic.account'].sudo().search([])
+        analytic_account_tag = request.env['account.analytic.tag'].sudo().search([])
+        paid_id = request.env['hr.expense']
+
+        autofill_data = {
+            'user': userid,
+            'r_product': r_product,
+            'cdate': cdate,
+            'currency_name': currency_name,
+            'tax_name': tax_name,
+            'analytic_account': analytic_account,
+            'analytic_account_tag': analytic_account_tag,
+            'account_id': account_id,
+            'account_id_def': account_id_def,
+            'paid_id': paid_id,
+        }
+        if kw:
+
+            tax_ids = int(kw.get('tax_name'))
+            product_name = int(kw.get('r_product'))
+            accountid = int(kw.get('account_id'))
+            analytic_acc = int(kw.get('analytic_account'))
+            analytic_tagid = int(kw.get('analytic_account_tag'))
+            vals = {
+                'name': kw.get('expname'),
+                'product_id': product_name,
+                'tax_ids': [(4, tax_ids)],
+                'reference': kw.get('ref'),
+                'account_id': accountid,
+                'analytic_account_id': analytic_acc,
+                'analytic_tag_ids': [(4, analytic_tagid)],
+                'unit_amount': 1,
+            }
+            create_record = request.env['hr.expense'].sudo().create(vals)
+
+        return http.request.render('travel_requisition.create_my_reimbursement', autofill_data)
 
 
 class MyExpenseCustomerPortal(CustomerPortal):
---
+++ travel_requisition/static/src/js/portal.js
@@ -0,0 +1,13 @@
+//this is for drop menu of checkout page
+//$(document).on('change', '#r_product', function() {
+//    var product = $(this).val();
+//    console.log(product)
+//
+//});
+
+//var base = document.getElementById("r_product").onchange = function() {myFunction()};
+//console.log(base)
+//function myFunction() {
+//  var product = $(this).val();
+//  console.log(product)
+//}
