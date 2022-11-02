075a6a113283c02f8515167a7679336382c2cc7a
amey204 <amey.planetodoo@gmail.com>
2022-11-02 12:19:45 +0530

    add changes

--- travel_requisition/controllers/my_reimbursement_portal.py
+++ travel_requisition/controllers/my_reimbursement_portal.py
@@ -34,24 +34,40 @@ class AllMyExpense(http.Controller):
 
     @http.route('/create/MyReimbursement', website=True, auth='public')
     def myreimbursement_form(self, **kw):
+        # get current login user
         userid = request.env.user.employee_id
 
+        # get products based on search filter from many2one
         r_product = request.env['product.product'].sudo().search(
             [('travel_requisition', '=', False), ('can_be_expensed', '=', True)])
+
+        # get current date
         cdate = date.today()
 
+        # get default selected account based on search filter from many 2 one
         account_id_def = request.env['account.account'].sudo().search(
             [('internal_type', '=', 'other'), ('code', '=', '210700')])
+        # get all account list based on search filter from many 2 one
         account_id = request.env['account.account'].sudo().search([('internal_type', '=', 'other')])
 
+        # get currency from many 2 one
         currency_name = request.env['res.currency'].sudo().search([])
+
+        # get all account list based on search filter from many 2 many
         tax_name = request.env['account.tax'].sudo().search(
             [('type_tax_use', '=', 'purchase'), ('price_include', '=', True)])
+
+        # get all analytic account list based on search filter from many 2 one
         analytic_account = request.env['account.analytic.account'].sudo().search([])
+
+        # get all account list based on search filter from many 2 many
         analytic_account_tag = request.env['account.analytic.tag'].sudo().search([])
+
+        # get field for getting paid by value
         paid_id = request.env['hr.expense']
 
         autofill_data = {
+            # 'module_fields_name' : defined_fields_name
             'user': userid,
             'r_product': r_product,
             'cdate': cdate,
@@ -65,41 +81,57 @@ class AllMyExpense(http.Controller):
         }
         if kw:
 
+            # writing if condition to get value from many 2 one if value not present then pass false to the field
             if kw.get('tax_name'):
+                # here tax_name many 2 many getting id in string is convert into integer and store in field
                 tax_ids = int(kw.get('tax_name'))
             else:
-                tax_ids = None
+                # if tax_name not select then pass false
+                tax_ids = False
 
             if kw.get('r_product'):
+                # here r_product many 2 one getting id in string is convert into integer and store in field
                 product_name = int(kw.get('r_product'))
             else:
-                product_name = None
+                # if product not select then pass false
+                product_name = False
 
             if kw.get('account_id'):
                 accountid = int(kw.get('account_id'))
             else:
-                accountid = None
+                accountid = False
 
             if kw.get('analytic_account'):
                 analytic_acc = int(kw.get('analytic_account'))
             else:
-                analytic_acc = None
+                analytic_acc = False
 
             if kw.get('analytic_account_tag'):
+                # here analytic_account_tag many 2 many getting id in string is convert into integer and store in field
                 analytic_tagid = int(kw.get('analytic_account_tag'))
             else:
-                analytic_tagid = None
+                # if analytic_account_tag not select then pass false
+                analytic_tagid = False
 
             vals = {
                 'name': kw.get('expname'),
                 'product_id': product_name,
-                'tax_ids': [(4, tax_ids)],
+
+                # this if condition and security condition is for many 2 many field
+                'tax_ids': False if tax_ids is False else [(4, tax_ids)],
+
                 'reference': kw.get('ref'),
                 'account_id': accountid,
                 'analytic_account_id': analytic_acc,
-                'analytic_tag_ids': [(4, analytic_tagid)],
+
+                # this if condition and security condition is for many 2 many field
+                'analytic_tag_ids': False if analytic_tagid is False else [(4, analytic_tagid)],
+
+                # don't know about this field, this field shows mandatory therefore i pass 1
                 'unit_amount': 1,
             }
+
+            # create method override to create record from form
             create_record = request.env['hr.expense'].sudo().create(vals)
 
         return http.request.render('travel_requisition.create_my_reimbursement', autofill_data)
